acf1d0c2a02bda55b64633d77ca70f24cecba903
WarringOG <53786241+WarringOG@users.noreply.github.com>
2020-06-10 20:31:13 -0400

    Update MapStorage.java

--- src/main/java/com/warring/library/storage/MapStorage.java
+++ src/main/java/com/warring/library/storage/MapStorage.java
@@ -1,7 +1,7 @@
 package com.warring.library.storage;
 
 import com.google.common.collect.Maps;
-
+import lombok.Getter;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
