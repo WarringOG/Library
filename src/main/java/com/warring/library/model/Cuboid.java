c8215b978be4137675a9285ab849cc915949dd8c
WarringOG <53786241+WarringOG@users.noreply.github.com>
2020-06-15 22:58:51 -0400

    Update Cuboid.java

--- src/main/java/com/warring/library/model/Cuboid.java
+++ src/main/java/com/warring/library/model/Cuboid.java
@@ -5,6 +5,8 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Cuboid {
@@ -28,11 +30,36 @@ public class Cuboid {
         return RegionUtils.isWithinCuboid(p.getLocation(), primary, second);
     }
 
+    public boolean locationInCuboid(Location loc) {
+        return RegionUtils.isWithinCuboid(loc, primary, second);
+    }
+
+    public boolean blockInCuboid(Block b) {
+        return locationInCuboid(b.getLocation());
+    }
+
     public Block[] getBlocks() {
         return RegionUtils.getBlocks(primary, second);
     }
 
     public Set<Location> getBox() {
         return RegionUtils.getBoundingBox(primary, second);
+    }
+
+    public Set<Block> cuboidEdges() {
+        Set<Block> currentList = new HashSet<>();
+        int dx = primary.getBlockX() - second.getBlockX();
+        int dy = primary.getBlockY() - second.getBlockY();
+        int dz = primary.getBlockZ() - second.getBlockZ();
+
+        for (int x = 0; x < dx; ++x) {
+            for (int z = 0; z < dz; z++) {
+                for (int y = 0; y < dy; y++) {
+                    Location loc = new Location(primary.getWorld(), primary.getBlockX() - x, primary.getBlockY() - y, primary.getBlockZ() - z);
+                    currentList.add(loc.getBlock());
+                }
+            }
+        }
+        return currentList;
     }
 }
